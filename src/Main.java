aabd68ed6113891ac8dafb6b778b784a7b4a53a7
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-06 17:23:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //method- a block of code that is executed whenever it is called upon
+
+        String name = "Bro";
+
+        hello(name);
+
+    }
+
+    static void hello(String name){
+        System.out.println("Hello  " + name);
+    }
+}
